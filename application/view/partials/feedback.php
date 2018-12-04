88c8b0776763ba0fea210cb831c55094290c16a7
Daniel Buendia <danielbuendiasmr@gmail.com>
2018-12-04 23:28:05 +0000

    Nada con fallos

--- application/view/partials/feedback.php
+++ application/view/partials/feedback.php
@@ -1,17 +1,17 @@
-<?php if (! is_null(Session::get('feedback_negative')) && count(Session::get('feedback_negative'))>0) : ?>
+<?php if (! is_null(\Mini\Core\Session::get('feedback_negative')) && count(\Mini\Core\Session::get('feedback_negative'))>0) : ?>
     <div class="errorf">
         <ul>
-            <?php foreach (Session::get('feedback_negative') as $error) : ?>
+            <?php foreach (\Mini\Core\Session::get('feedback_negative') as $error) : ?>
                 <li><?= $error ?></li>
             <?php endforeach ?>
         </ul>
     </div>
 <?php endif ?>
 
-<?php if (! is_null(Session::get('feedback_positive')) && count(Session::get('feedback_positive'))>0) : ?>
+<?php if (! is_null(\Mini\Core\Session::get('feedback_positive')) && count(\Mini\Core\Session::get('feedback_positive'))>0) : ?>
     <div class="exitof">
         <ul>
-            <?php foreach (Session::get('feedback_positive') as $error) : ?>
+            <?php foreach (\Mini\Core\Session::get('feedback_positive') as $error) : ?>
                 <li><?= $error ?></li>
             <?php endforeach ?>
         </ul>
